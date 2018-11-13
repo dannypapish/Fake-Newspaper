13de7b8e18660e62779afa44baf46455e9d843d5
Danny Papish <dannypapish@gmail.com>
2018-11-12 23:37:52 -0500

    newspaper

---
+++ newspaper.js
@@ -0,0 +1,11 @@
+  var formHeading = document.getElementById('form-title'),
+	name="Danny";
+
+	console.log("This is what WAS inside the heading: " + formHeading.innerText);
+
+var myDataAtrr = document.querySelectorAll('[data-something]')
+
+
+
+document. getElementById('form-title').onclick = function(){  alert('you clicked me')
+};
